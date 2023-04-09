2f0b62f5db72abc1a19fe2318701e69dca8647b7
Ihor Pol <igorxlonline24@gmail.com>
2023-04-10 02:00:00 +0300

    remove unnecessary comments

--- Controllers/BotController.cs
+++ Controllers/BotController.cs
@@ -18,14 +18,14 @@ namespace FinancialAdvisorTelegramBot.Controllers
 
 
         [HttpPost]
-        public async void Post(Update update) //Сюда будут приходить апдейты
+        public async void Post(Update update)
         {
             Console.WriteLine(update.Message?.Text);
             await _bot.BotClient.SendTextMessageAsync(update.Message.Chat.Id, "Hi!");
         }
         
         [HttpGet]
-        public string Get() // View
+        public string Get()
         {
             return "Telegram bot was started";
         }
